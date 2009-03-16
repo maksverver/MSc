ee0f915585ca718ff075e09ae03f1c76be6673f9
Maks Verver <maksverver@geocities.com>
2009-03-16 14:56:14 +0100

    Removed obsolete mCRL2 test case.

--- src/LinearLiftingStrategy.h
+++ src/LinearLiftingStrategy.h
@@ -18,8 +18,8 @@ public:
     bool backward() const { return backward_; }
 
 private:
-    const bool backward_;       /*!< indicates the direction to move */
-    verti failed_lifts_;        /*!< number of consecutive failed lift attempts */
+    const bool backward_;       //!< indicates the direction to move
+    verti failed_lifts_;        //!< number of consecutive failed lift attempts
 };
 
 #endif /* ndef LINEAR_LIFTING_STRATEGY_H_INCLUDED */
