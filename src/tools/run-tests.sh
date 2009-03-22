182844777b0da9f389b24d7e3ac117d47aa07c26
Maks Verver <verver@twickel.ewi.utwente.nl>
2009-03-22 21:58:20 +0100

    Updated script to run tests.

--- src/tools/run-tests.sh
+++ src/tools/run-tests.sh
@@ -13,9 +13,9 @@ focus_D focus_BD
 #input=../../tests/onebit_1_infrw.pbes
 format=raw
 input=onebit_1_infrw.raw
-tmp=run.tmp
+tmp=`mktemp`
+solver='./main'
 
-echo $runs
 for run in $runs
 do
 	strat=`echo $run | cut -f1 -d_`
@@ -39,11 +39,13 @@ do
 		args="$args --scc"
 	fi
 
-	./main --input "$format" --strategy $strat $args < "$input" 2> "$tmp"
-	lift_total=`grep 'Total lift attempts' "$tmp" | cut -c35-`
-	lift_success=`grep 'Succesful lift attempts' "$tmp" | cut -c35-`
-	lift_fail=$((lift_total - lift_success))
+	$solver --input "$format" --strategy $strat $args < "$input" 2> "$tmp"
+	lift_total=`grep 'Total lifting attempts:' "$tmp" | cut -c40-`
+	lift_success=`grep 'Lifting attempts succeeded:' "$tmp" | cut -c40-`
+	lift_failed=`grep  'Lifting attempts failed:' "$tmp" | cut -c40-`
 	time_used=`grep 'Time used to solve' "$tmp" | cut -c35-`
 	printf "%-15s  %'14d  %'14d  %'14d  %9s\n" \
-		"`echo $run | tr _ \ `" $lift_fail $lift_success $lift_total "$time_used"
+		"`echo $run | tr _ \ `" $lift_failed $lift_success $lift_total "$time_used"
 done
+
+rm -f "$tmp"
