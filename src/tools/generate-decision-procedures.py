14ae1caeefa1629ba6b3d72480deeb5ad8af4424
Maks Verver <verver@weldam.(none)>
2011-04-08 00:36:46 +0200

    Modified mu-calculus generator so it generates labeled formulae.

--- src/tools/generate-decision-procedures.py
+++ src/tools/generate-decision-procedures.py
@@ -10,13 +10,13 @@ for n in range(2,8):
 		psi += ["mu","nu"][m%2] + " X" + str(m + 1) + "."
 	for m in range(n):
 		if m > 0: psi += " & "
-		psi += "(q" + str(m + 1) + " | ()(X" + str(m + 1)
+		psi += "(q" + str(m + 1) + " | <a>(X" + str(m + 1)
 	psi += "))"*n
 	print("#psi" + str(n) + " := " + psi + ";")
 	print("#phi" + str(n) + " := #psi" + str(n) + " | !#psi" + str(n) + ";")
 
 for n in [10,50,100,500,1000,2000]:
-	phi_prime = "(nu X." +  "(q&()"*(2*n) + "(!q&()X)" + ")"*(2*n) + ")" \
-	          + " ==> nu Z.mu Y.(!q&()Z)|(q&()(q&()Y))"
+	phi_prime = "(nu X." +  "(q&<a>"*(2*n) + "(!q&<a>X)" + ")"*(2*n) + ")" \
+	          + " ==> nu Z.mu Y.(!q&<a>Z)|(q&<a>(q&<a>Y))"
 	# N.B. relabeled from phi' to chi:
 	print("#chi" + str(n) + " := " + phi_prime + ";")
