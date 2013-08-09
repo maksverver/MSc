8d9e3ae664f1fc8ebe39907bf316413d2efb98ab
Maks Verver <maksverver@geocities.com>
2013-08-09 18:53:17 +0200

    Updated thesis.

--- src/Decimal.cc
+++ src/Decimal.cc
@@ -7,7 +7,7 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-#include "decimal.h"
+#include "Decimal.h"
 #include <vector>
 
 Decimal operator+(const Decimal &s, const Decimal &t)
